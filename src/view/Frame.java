7720b49e082a33cd57d0c739f8294429cddea310
thatsmellything <thesmelliestman@gmail.com>
2019-09-11 11:05:16 -0600

    Update Frame.java

--- src/view/Frame.java
+++ src/view/Frame.java
@@ -1,5 +1,31 @@
 package view;
 
-public class Frame {
+import controller.Controller;
+import javax.swing.JFrame;
+
+public class Frame extends JFrame{
+	
+	private Controller appController;
+	private Panel appPanel;
+	
+	
+	public Frame(Controller appController)
+	{
+		super();
+		this.appController = appController;
+		this.appPanel = new Panel(appController);
+		
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setContentPane(appPanel);
+		this.setSize(1024, 768);
+		this.setTitle("JaVuls ver 0.0.1");
+		this.setResizable(false);
+		this.setVisible(true);
+	}
+	
 
 }
