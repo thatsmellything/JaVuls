1fb9a399d916efd722a39122f4295ed9630463a1
thatsmellything <thesmelliestman@gmail.com>
2019-09-11 11:37:41 -0600

    Update Panel.java

--- src/view/Panel.java
+++ src/view/Panel.java
@@ -14,11 +14,82 @@ import controller.IOController;
 public class Panel extends JPanel
 {
 
+	//Text fields
+	private JTextField IPInput;
+	private JTextField URLInput;
+	private JTextField NickName;
 	
 	
+	//Buttons
+	private JButton PortScanner;
+	private JButton Pinger;
+	private JButton GeoLocator;
+	private JButton ArpScan;
 	
 	
+	//Text Areas
+	private JTextArea Notes;
 	
+	//Button Panels
+	private JPanel buttonPanelLeft;
+	
+	//Controllers and panels
+	private Controller appController;
+	private SpringLayout appLayout;
+	
+	
+	public Panel(Controller appController)
+	{
+		super();
+		this.appController = appController;
+		appLayout = new SpringLayout();
+		
+		
+		
+		
+		
+		
+		setupLayout();
+		setupPanel();
+		setupScrollPane();
+		setupListeners();
+		setupButtonPanelLeft();
+	}
+	
+	public void setupButtonPanelLeft()
+	{
+		buttonPanelLeft.add(PortScanner);
+		buttonPanelLeft.add(Pinger);
+		buttonPanelLeft.add(GeoLocator);
+		buttonPanelLeft.add(ArpScan);
+	}
+	
+	private void setupPanel()
+	{
+		this.setLayout(appLayout);
+		this.setPreferredSize(new Dimension(1024, 768));
+	}
+	
+	private void setupListeners()
+	{
+		PortScanner.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				
+			}
+		});
+	}
+	
+	private void setupScrollPane()
+	{
+		
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
 	
 	
 	
