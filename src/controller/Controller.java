48a11ae4e86eb6cf592f7c8e7c0ca6b28bd4155e
thatsmellything <thesmelliestman@gmail.com>
2019-09-11 11:43:01 -0600

    Update Controller.java

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,9 +1,37 @@
 package controller;
 
+import view.*;
+import javax.swing.JOptionPane;
+
+
+
+
 public class Controller {
 
+	private Frame appFrame;
+	
+	public Controller()
+	{
+		appFrame = new Frame(this);
+	}
+	
 	public void start()
 	{
 		
+	}
+	
+	private void close()
+	{
+		System.exit(0);
+	}
+	
+	public void handleErrors(Exception error)
+	{
+		JOptionPane.showMessageDialog(appFrame, error.getMessage());
+	}
+	
+	public Frame getAppFrame()
+	{
+		return appFrame;
 	}
 }
