95252045f725766fea0ed0f399e58c2c37836617
thatsmellything <thesmelliestman@gmail.com>
2019-10-21 11:32:41 -0600

    Create JFX.java

---
+++ src/view/JFX.java
@@ -0,0 +1,26 @@
+package view;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class JFX extends Application{
+	
+	
+	
+	public void start(Stage primaryStage)
+	{
+		//Create the buttons needed
+		Button IPLookup = new Button();
+		Button ScanIPPorts = new Button();
+		
+		//Create the pane
+		GridPane pane = new GridPane();
+		pane.setAlignment(Pos.CENTER);
+		
+		
+	}
+
+}
