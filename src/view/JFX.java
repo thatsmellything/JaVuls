f9ec3946fec48a0d29fb75e3209071dddea024c2
thatsmellything <thesmelliestman@gmail.com>
2019-10-24 14:48:57 -0600

    Update JFX.java

--- src/view/JFX.java
+++ src/view/JFX.java
@@ -2,7 +2,9 @@ package view;
 
 import javafx.application.Application;
 import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -12,15 +14,38 @@ public class JFX extends Application{
 	
 	public void start(Stage primaryStage)
 	{
-		//Create the buttons needed
-		Button IPLookup = new Button();
-		Button ScanIPPorts = new Button();
 		
-		//Create the pane
+		
+		//Create the gridpane
 		GridPane pane = new GridPane();
 		pane.setAlignment(Pos.CENTER);
 		
+		//Set title for the stage
+		primaryStage.setTitle("JaVuls");
 		
+		//Create the scene
+		Scene primaryScene = new Scene(pane, 500, 500);
+		
+				//Create the buttons needed
+				Button IPLookup = new Button("Scan IP Address");
+				Button ScanIPPorts = new Button("Scan IP Ports");
+					//Add buttons to grid pane
+					pane.add(IPLookup, 0, 1, 1, 1);
+				
+				//Create the textFields needed
+				TextField IPAddress = new TextField();
+					//add textFields to grid pane
+					pane.add(IPAddress, 0, 0, 1, 1);
+					
+		//Show the stage and jazz
+		primaryStage.setScene(primaryScene);
+		primaryStage.show();
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		launch(args);
 	}
 
 }
