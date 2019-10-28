c090c5556a3573202a1bb218d706260d7e50a635
thatsmellything <thesmelliestman@gmail.com>
2019-10-28 12:45:32 -0600

    Add files via upload

---
+++ JaVuls1.0.1/src/applications/YourIPLookup.java
@@ -0,0 +1,29 @@
+package applications;
+
+public class YourIPLookup {
+	// static String LocalIP = getIpAddress(null);
+	  
+	   /* public static String getLocalIP() throws UnknownHostException
+	    {
+	        InetAddress inetAddress = InetAddress.getLocalHost();
+	        System.out.println("IP Address:- " + inetAddress.getHostAddress());
+	        System.out.println("Host Name:- " + inetAddress.getHostName());
+	        return LocalIP;
+	    }
+	    */
+	    
+	    
+	    private static String getIpAddress(byte[] rawBytes) {
+	        int i = 4;
+	        StringBuilder ipAddress = new StringBuilder();
+	        for (byte raw : rawBytes) {
+	            ipAddress.append(raw & 0xFF);
+	            if (--i > 0) {
+	                ipAddress.append(".");
+	            }
+	        }
+	        return ipAddress.toString();
+	    }
+	    
+
+}
---
+++ JaVuls1.0.1/src/controller/javaFXTester.java
@@ -0,0 +1,47 @@
+package controller;
+
+import java.awt.Dimension;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class javaFXTester extends Application
+{
+
+	@Override
+	public void start(Stage primaryStage)
+	{
+		
+		//Create pane
+		GridPane pane = new GridPane();
+		//pane.setAlignment(Pos.CENTER);
+		
+		pane.setHgap(5);
+		pane.setVgap(5);
+		Button test1 = new Button("test1");
+		Button test2 = new Button("test2");
+		pane.add(test1, 5, 5);
+		pane.add(test2, 0, 1);
+		
+		
+		//Create scene
+		Scene scene = new Scene(pane, 1024, 768);
+		
+		
+		
+		
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	
+	public static void main(String[] args)
+	{
+		System.out.println("HI");
+		launch(args);
+	}
+
+}
