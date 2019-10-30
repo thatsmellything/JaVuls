1c9f62f16338ff2d5a36400b61553a69fc4288cc
thatsmellything <thesmelliestman@gmail.com>
2019-10-30 00:01:32 -0600

    Update AddABreak.java

--- src/controller/AddABreak.java
+++ src/controller/AddABreak.java
@@ -6,7 +6,7 @@ public class AddABreak {
 	public void addFive()
 	{
 		try {
-		    
+			System.out.println("Taking 5 second break just you hold up! :)");
 			TimeUnit.SECONDS.sleep(5);
 		} catch (InterruptedException ie) {
 			Thread.currentThread().interrupt();
