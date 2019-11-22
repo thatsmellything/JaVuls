4b976a3005f44163ceb8fc1dd7bc7eb83c716024
thatsmellything <thesmelliestman@gmail.com>
2019-11-22 12:02:24 -0700

    Update IPLookup.java

--- src/applications/IPLookup.java
+++ src/applications/IPLookup.java
@@ -1,5 +1,56 @@
 package applications;
 
+// Java program to demonstrate 
+// how to fetch public IP Address 
+import java.net.*; 
+//import java.*; 
+
 public class IPLookup {
 
-}
+
+	  
+	 
+	    public static void main(String args[]) 
+	        throws UnknownHostException 
+	    { 
+	        // The URL for which IP address needs to be fetched 
+	        String s = "https:// www.google.com/"; 
+	  
+	        try { 
+	            // Fetch IP address by getByName() 
+	            InetAddress ip = InetAddress.getByName(new URL(s) 
+	                                                       .getHost()); 
+	  
+	            // Print the IP address 
+	            System.out.println("Public IP Address of: " + ip); 
+	        } 
+	        catch (MalformedURLException e) { 
+	            // It means the URL is invalid 
+	            System.out.println("Invalid URL"); 
+	        } 
+	        
+	    } 
+	    
+	    public String websiteIP(String s) throws UnknownHostException
+	    {
+	    	try { 
+	            // Fetch IP address by getByName() 
+	            InetAddress ip = InetAddress.getByName(new URL(s) 
+	                                                       .getHost()); 
+	  
+	            // Print the IP address 
+	            System.out.println("Public IP Address of: " + ip); 
+	            String returnMe = "Public IP Address of: " + ip;
+	            return returnMe;
+	        } 
+	        catch (MalformedURLException e) { 
+	            // It means the URL is invalid 
+	            System.out.println("Invalid URL"); 
+	            String returnMe = "Invalid input";
+	            return returnMe;
+	        } 
+	    }
+	} 
+
+
+
