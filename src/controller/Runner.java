807906c8ac9039c44648fc5f210244e8a1fdd1f4
thatsmellything <jensen.judkins@aggiemail.usu.edu>
2020-10-12 22:49:45 -0600

    Messed around with multithreading. I think i understand the gist just not perfectly

--- src/controller/Runner.java
+++ src/controller/Runner.java
@@ -1,22 +1,30 @@
 package controller;
 
 
+import java.util.concurrent.ThreadLocalRandom;
+
 import testFiles.*;
 import view.*;
 
 public class Runner{
 	public static void main(String[] args)
 	{
-
-
-
-
+		Thread startGUI = new Thread(new Runnable() {
+			public void run() {
+				JFX app = new JFX();
+				app.main(args);
+			}
+		});
+		System.out.println("How far will it copy?");
+		startGUI.start();
 		
-
-
 		
-		JFX app = new JFX();
-		app.main(args);
+		//int unboundedRandomValue = ThreadLocalRandom.current().nextInt();
+		//System.out.println(unboundedRandomValue);
+		//testBruteForce();
+		
+		//JFX app = new JFX();
+		//app.main(args);
 	}
 	public static void testBruteForce()
 	{
@@ -25,6 +33,69 @@ public class Runner{
 		String stringlad =  "900150983CD24FB0D6963F7D28E17F72";
 		String crackboi = BruteCrack.test(stringlad);
 		System.out.println(crackboi);
+		
+		
+		Thread test1 = new Thread(new Runnable() {
+			public void run() {
+				String stringlad =  "900150983CD24FB0D6963F7D28E17F72";
+				String crackboi = BruteCrack.test(stringlad);
+				System.out.println(crackboi);
+			}
+		});
+		test1.start();
+		Thread test2 = new Thread(new Runnable() {
+			public void run() {
+				String stringlad =  "900150983CD24FB0D6963F7D28E17F72";
+				String crackboi = BruteCrack.test(stringlad);
+				System.out.println(crackboi);
+			}
+		});
+		test2.start();
+		Thread test3 = new Thread(new Runnable() {
+			public void run() {
+				String stringlad =  "900150983CD24FB0D6963F7D28E17F72";
+				String crackboi = BruteCrack.test(stringlad);
+				System.out.println(crackboi);
+			}
+		});
+		test3.start();
+		Thread test4 = new Thread(new Runnable() {
+			public void run() {
+				String stringlad =  "900150983CD24FB0D6963F7D28E17F72";
+				String crackboi = BruteCrack.test(stringlad);
+				System.out.println(crackboi);
+			}
+		});
+		test4.start();
+		Thread test5 = new Thread(new Runnable() {
+			public void run() {
+				String stringlad =  "900150983CD24FB0D6963F7D28E17F72";
+				String crackboi = BruteCrack.test(stringlad);
+				System.out.println(crackboi);
+			}
+		});
+		test5.start();
+		Thread test6 = new Thread(new Runnable() {
+			public void run() {
+				String stringlad =  "900150983CD24FB0D6963F7D28E17F72";
+				String crackboi = BruteCrack.test(stringlad);
+				System.out.println(crackboi);
+			}
+		});
+		test6.start();
+		Thread test7 = new Thread(new Runnable() {
+			public void run() {
+				String stringlad =  "900150983CD24FB0D6963F7D28E17F72";
+				String crackboi = BruteCrack.test(stringlad);
+				System.out.println(crackboi);
+			}
+		});
+		test7.start();
+
+		
+		
+		
+		
 	}
 	public static void testHttpServer()
 	{
